1d5ed9825897aa4baec877e0ce35c07ffb8301f2
nu11secur1ty <systadmi@abv.bg>
2016-09-14 16:22:49 -0400

    Update pulseaudio.sh

--- pulseaudio.sh
+++ pulseaudio.sh
@@ -11,7 +11,7 @@ echo -e '#!/bin/bash\ntimeout 10 pulseaudio -D\nsleep 15\exit 0;' > $file
 chmod 0500 $file
 mkdir -p /root/.config/autostart/
          file=/root/.config/autostart/pulseaudioscript.sh.desktop; [ -e $file ] && cp -n $file{,.bkup}
-echo -e '\n[Desktop Entry]\nType=Application\nExec=/usr/local/bin/pulseaudio.sh\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart- enabled=true\nName[en_US]=pulseaudio\nName=pulseaudio\nComment[en_US]=\nComment=' > $file
+echo -e '\n[Desktop Entry]\nType=Application\nExec=/usr/local/bin/pulseaudio.sh\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true\nName[en_US]=pulseaudio\nName=pulseaudio\nComment[en_US]=\nComment=' > $file
       sleep 5;
 echo "After reboot your system everything will be ok with your audio controller. Have fun with nu11secur1ty -;)"
 exit 0;
